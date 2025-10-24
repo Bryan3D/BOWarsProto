1b243a3c8710d3a294724abff5d12d2fcb1bb166
Bryan Alec Quero <bryanq2008@live.com>
2025-10-24 19:30:14 -0400

    first commit

---
+++ Source/BOWarsProto/BOWarsProto.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BOWarsProto.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BOWarsProto, "BOWarsProto" );
